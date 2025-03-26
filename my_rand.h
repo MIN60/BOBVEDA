f257c4f13b64c6f09226397570e2452e36ef7d2a
Ong <ngmryngx@gmail.com>
2025-03-27 08:52:01 +0900

    Chore/#3: 동철님 코드 + 삭제할 사람 성별 바꿔 표시

---
+++ my_rand.h
@@ -0,0 +1,6 @@
+#pragma once
+
+typedef struct MEMBER MEMBER;
+int my_rand();
+void my_srand(unsigned int seed);
+void shuffle(int* arr, int size);
---
+++ struct2.h
@@ -0,0 +1,12 @@
+// struct.h ÆÄÀÏ
+#ifndef _STRUCT_H_
+#define _STRUCT_H_
+
+typedef struct Person {
+    wchar_t name[20];
+    char gender;
+    unsigned char id;
+    unsigned int prev_team;
+} Person;
+
+#endif
