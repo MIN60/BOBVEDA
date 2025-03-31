a19d6e907c77d4c2696dc34f0f6ea93b60c405c6
MIN <whitedaisy12759@gmail.com>
2025-03-31 18:46:09 +0900

    Del/#2: 파싱함수 변경

--- flask/main.py
+++ flask/main.py
@@ -31,23 +31,16 @@ os.makedirs(RESULT_FOLDER, exist_ok=True)
 
 #슬랙 명령어 파싱
 def parse_command(text):
-    div_group = None #나눌 조
-    remove_p = [] # 뺄사람ㅁ
+    remove_p = []
 
-    parts = text.split('-', 1) # -ㄱㅁㅈ
+    if text and '-' in text:
+        parts = text.split('-', 1)
+        exclude_text = parts[1].strip()
+        remove_p = [
+            name.strip() for name in exclude_text.split(',') if name.strip()
+        ]
 
-    if parts and parts[0].strip(): #나눌 조 입력했는지
-        try:
-            div_group = int(parts[0].strip()) #그룹수가 문자열로 나와서 정수로 바꿔줘야됨
-        except ValueError:
-            return None, []
-
-    if len(parts) > 1 and parts[1].strip(): #제외할 사람 있는지
-        exclude_text = parts[1].strip() 
-        remove_p = [name.strip() for name in exclude_text.split(',')]
-
-    #그룹수, 제거자 파싱
-    return div_group, remove_p
+    return remove_p
 
 # 파일 업로드 리마인드 (30초 뒤에도 안 올리면 알림림)
 def remind_file(user_id):
